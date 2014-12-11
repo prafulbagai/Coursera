720a017485c59b5bc4c07b8282d43f8033b62cf5
Praful <praful.bagai1991@gmail.com>
2014-12-11 22:22:21 +0530

    Added Navbar and its respective functionalites

--- group10/wsgi.py
+++ group10/wsgi.py
@@ -13,5 +13,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "group10.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+#from dj_static import Cling
+#application = Cling(get_wsgi_application())
