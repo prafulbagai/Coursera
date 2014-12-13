b7e4db724f65af22cadc77b67be864cfbe317f27
Praful <praful.bagai1991@gmail.com>
2014-12-13 22:20:14 +0530

    Responsiveness

--- static/js/main.js
+++ static/js/main.js
@@ -53,29 +53,38 @@ function ajax_request(filter,search_val ){
 		success : function(result){
 			$("#courses").empty()
 			var text = ""
-			for (i = 0; i< result.length; i++){
-				text += "<div class='row-fluid'>" +
-							"<div class='span2'>" + 
-								"<img src='/static/img/group10/" + result[i][0] + "'>" + 
-							"</div>"+
-
-							"<div class='span8'>" +
-								"<a href='" + result[i][1] + "'>" + result[i][2] + "</a><br>" + 
-								"<a href='/course/" + result[i][3] + "'>" + result[i][4] + "</a><br>" +
-								"With "
-				for (j = 0; j < result[i][7].length; j++){
-					text += "<a href='/instructors/" + result[i][7][j][1] + "'>" + result[i][7][j][0] + "</a> ,"
-				}
-
-				text += "</div>" + 
-							"<div class='span2'>" +
-							 	result[i][5] + "<br>" + result[i][6] + 
-						"</div>" + 
-					"</div>" + 
-					"<hr>"
+			if (result.length == 0){
+				text += "<h4 style= 'text-align:center;padding:40px 0px 40px 0px ;'>No Course Found with above search.</h4>"
+				$("#courses").html(text)
 			}
 
-			$("#courses").html(text)
+			else{
+				for (i = 0; i< result.length; i++){
+					text += "<div class='row-fluid hoverDiv' style='padding:10px 0px 10px 0px;'>" +
+								"<div class='span2'  style='padding:0px 10px 0px 10px'>" + 
+									"<img src='/static/img/group10/" + result[i][0] + "' class='course-img'>" + 
+								"</div>"+
+
+								"<div class='span8' style='padding-left:10px;'>" +
+									"<a href='" + result[i][1] + "'>" + result[i][2] + "</a><br>" + 
+									"<a href='/course/" + result[i][3] + "'>" + result[i][4] + "</a><br>" +
+									"<span class= 'visible-desktop'>" +
+									"With "
+					for (j = 0; j < result[i][7].length; j++){
+						text += "<a href='/instructors/" + result[i][7][j][1] + "'>" + result[i][7][j][0] + "</a> ,"
+					}
+
+					text += "</span></div>" + 
+								"<div class='span2' style='padding-left:10px;'>" +
+								 	"<div class='hidden-desktop'>" + result[i][5] + "(" + result[i][6] + ")</div>" + 
+								 	"<div class='visible-desktop'>" + result[i][5] + "<br>" + result[i][6] + "</div>" + 
+							"</div>" + 
+						"</div>" + 
+						"<hr>"
+				}
+
+				$("#courses").html(text)
+			}
 		}
 	});
 }
